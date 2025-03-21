882253b522dc26440787f3cab7dc22c92bc1b591
huynhsown <116697096+huynhsown@users.noreply.github.com>
2025-03-21 08:50:33 +0700

    Delete API/App_Food/src/main/java/com/example/app_food/Repository directory

--- API/App_Food/src/main/java/com/example/app_food/Repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.app_food.Repository;
-
-import com.example.app_food.Entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer>
-{
-
-}
--- API/App_Food/src/main/java/com/example/app_food/Repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.app_food.Repository;
-
-import com.example.app_food.Entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
