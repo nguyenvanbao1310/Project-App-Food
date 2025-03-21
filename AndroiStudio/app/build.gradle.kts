4788b5cd0eebd9a5b985fa7d9eddd4b8fff98b48
22110440 <157661962+22110440@users.noreply.github.com>
2025-03-21 07:50:28 +0700

    22110440- Hoàng Thanh Tri UI login

--- AndroiStudio/app/build.gradle.kts
+++ AndroiStudio/app/build.gradle.kts
@@ -38,6 +38,8 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:logging-interceptor:3.12.0")
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation ("androidx.activity:activity:1.6.1")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
